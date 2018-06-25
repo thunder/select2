f58d5931bb29ea3009330b202ab28957d35c0bcf
Christian Fritsch <chr.fritsch@gmx.net>
2018-06-25 17:00:01 +0200

    Imporove empty value handling

--- src/Element/Select2.php
+++ src/Element/Select2.php
@@ -16,15 +16,36 @@ class Select2 extends Select {
    * {@inheritdoc}
    */
   public static function processSelect(&$element, FormStateInterface $form_state, &$complete_form) {
-    $element = parent::processSelect($element, $form_state, $complete_form);
-
-    // Adding the select2 library.
-    $element['#attached']['library'][] = 'select2/select2';
+    $required = isset($element['#states']['required']) ? TRUE : $element['#required'];
+    $multiple = $element['#multiple'];
 
     // Defining the select2 configuration.
     $settings = [
-      'multiple' => $element['#multiple'],
+      'multiple' => $multiple,
+      'placeholder' => $required ? t('- Select -') : t('- None -'),
+      'allowClear' => !$multiple && !$required ? TRUE : FALSE,
     ];
+
+    if ($multiple) {
+      $element['#attributes']['multiple'] = 'multiple';
+      $element['#attributes']['name'] = $element['#name'] . '[]';
+    }
+    // A non-#multiple select needs special handling to prevent user agents from
+    // preselecting the first option without intention. #multiple select lists
+    // do not get an empty option, as it would not make sense, user
+    // interface-wise.
+    else {
+      if (!isset($element['#default_value'])) {
+        // The empty option is prepended to #options and purposively not merged
+        // to prevent another option in #options mistakenly using the same value
+        // as #empty_value.
+        $empty_option = ['' => ''];
+        $element['#options'] = $empty_option + $element['#options'];
+      }
+    }
+
+    // Adding the select2 library.
+    $element['#attached']['library'][] = 'select2/select2';
 
     $selector = $element['#attributes']['data-drupal-selector'];
     $element['#attached']['drupalSettings']['select2'][$selector] = $settings;

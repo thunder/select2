fe5fe782ffeef1570568e3bcbd6f1c2f5071318c
Christian Fritsch <chr.fritsch@gmx.net>
2018-07-17 10:10:04 +0200

    Adjusting

--- tests/src/Kernel/Element/Select2Test.php
+++ tests/src/Kernel/Element/Select2Test.php
@@ -29,14 +29,14 @@ class Select2Test extends KernelTestBase {
     ];
 
     $this->render($select);
-    $select2_js = $this->xpath("//script[contains(@src, 'modules/select2/js/select2.js')]");
-    $this->assertEqual(count($select2_js), 1);
+    $select2_js = $this->xpath("//script[contains(@src, 'select2/js/select2.js')]");
+    $this->assertEquals(1, count($select2_js));
     $select2_js = $this->xpath("//script[contains(@src, 'select2/dist/js/select2.min.js')]");
-    $this->assertEqual(count($select2_js), 1);
-    $select2_css = $this->xpath("//link[contains(@href, 'modules/select2/css/select2.seven.css')]");
-    $this->assertEqual(count($select2_css), 1);
-    $select2_css = $this->xpath("//link[contains(@href, 'modules/select2/css/select2.seven-overrides.css')]");
-    $this->assertEqual(count($select2_css), 1);
+    $this->assertEquals(1, count($select2_js));
+    $select2_css = $this->xpath("//link[contains(@href, 'select2/css/select2.seven.css')]");
+    $this->assertEquals(1, count($select2_css));
+    $select2_css = $this->xpath("//link[contains(@href, 'select2/css/select2.seven-overrides.css')]");
+    $this->assertEquals(1, count($select2_css));
   }
 
 }

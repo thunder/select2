91c30921f49f78603ddd4ccd234bc0350806d0a4
Christian Fritsch <chr.fritsch@gmx.net>
2018-07-18 16:20:58 +0200

    Start fixing autocreation in ajax calls

--- tests/src/FunctionalJavascript/Form/ElementTest.php
+++ tests/src/FunctionalJavascript/Form/ElementTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\select2\FunctionalJavascript\Form;
 
+use Drupal\entity_test\Entity\EntityTestMulRevPub;
 use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
 
 /**
@@ -14,7 +15,7 @@ class ElementTest extends WebDriverTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['select2_form_test'];
+  protected static $modules = ['select2_form_test', 'entity_test'];
 
   /**
    * Tests select2 optgroups.
@@ -28,6 +29,25 @@ class ElementTest extends WebDriverTestBase {
 
     $this->assertSession()->elementTextContains('css', '.select2-results__group', 'Baba');
     $this->assertSession()->elementTextContains('css', 'ul.select2-results__options li.select2-results__option ul.select2-results__options--nested li.select2-results__option', 'Nana');
+  }
+
+  /**
+   * Test that in-between ajax calls are not creating new entities.
+   */
+  public function testAjaxCallbacksInBetween() {
+
+    $page = $this->getSession()->getPage();
+    $this->drupalGet('/select2-ajax-form');
+
+    $this->click('.form-item-select2-ajax .select2-selection.select2-selection--multiple');
+    $this->assertNotEmpty($this->assertSession()->waitForElementVisible('css', '.select2-search__field'));
+
+    $page->find('css', '.select2-search__field')->setValue('New value');
+    $page->find('css', '.select2-results__option--highlighted')->click();
+    $page->pressButton('Call ajax');
+
+    $this->assertEmpty(EntityTestMulRevPub::loadMultiple());
+
   }
 
 }

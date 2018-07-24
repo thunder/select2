b6c1ae5a28cce00d44bfc748974db815669f7e44
Daniel Bosen <daniel.bosen@burda.com>
2018-07-24 12:41:10 +0200

    create directory

--- scripts/travis/download-library.sh
+++ scripts/travis/download-library.sh
@@ -10,4 +10,5 @@ esac
 
 wget https://github.com/select2/select2/archive/master.zip
 unzip  master.zip
+mkdir /tmp/test-install/${docroot}/libraries
 mv select2-master /tmp/test-install/${docroot}/libraries/select2

1ac1b9ec25aa29ed77ad121b70132fceb890a303
Christian Fritsch <chr.fritsch@gmx.net>
2018-09-19 16:32:36 +0200

    More tests

--- tests/src/Kernel/Element/Select2Test.php
+++ tests/src/Kernel/Element/Select2Test.php
@@ -47,8 +47,7 @@ class Select2Test extends KernelTestBase {
       '#attributes' => ['data-drupal-selector' => 'field-foo'],
     ];
     $this->render($select);
-    $select = $this->xpath('//select[@data-drupal-selector="field-foo"]/option[@value=""]');
-    $this->assertEquals(1, count($select));
+    $this->assertOptionExists('field-foo', '');
 
     $select = [
       '#type' => 'select2',
@@ -59,7 +58,52 @@ class Select2Test extends KernelTestBase {
       '#name' => 'field_foo',
     ];
     $this->render($select);
-    $select = $this->xpath('//select[@data-drupal-selector="field-foo"]/option[@value=""]');
+    $this->assertOptionNotExists('field-foo', '');
+  }
+
+  /**
+   * Tests that in autocomplete are only the default options rendered.
+   */
+  public function testAutocompleteOptions() {
+    $select = [
+      '#type' => 'select2',
+      '#options' => ['foo' => 'Foo', 'bar' => 'Bar', 'foo_bar' => 'FooBar'],
+      '#default_value' => ['foo'],
+      '#autocomplete' => TRUE,
+      '#target_type' => 'node',
+      '#required' => FALSE,
+      '#attributes' => ['data-drupal-selector' => 'field-foo'],
+    ];
+    $this->render($select);
+
+    $this->assertOptionExists('field-foo', 'foo');
+    $this->assertOptionNotExists('field-foo', 'bar');
+    $this->assertOptionNotExists('field-foo', 'foo_bar');
+  }
+
+  /**
+   * Tests that a select option exists.
+   *
+   * @param string $selector
+   *   The data-drupal-selector.
+   * @param string $value
+   *   The value of the option.
+   */
+  protected function assertOptionExists($selector, $value) {
+    $select = $this->xpath('//select[@data-drupal-selector="' . $selector . '"]/option[@value="' . $value . '"]');
+    $this->assertEquals(1, count($select));
+  }
+
+  /**
+   * Tests that a select option not exists.
+   *
+   * @param string $selector
+   *   The data-drupal-selector.
+   * @param string $value
+   *   The value of the option.
+   */
+  protected function assertOptionNotExists($selector, $value) {
+    $select = $this->xpath('//select[@data-drupal-selector="' . $selector . '"]/option[@value="' . $value . '"]');
     $this->assertEquals(0, count($select));
   }
 

bda13a464c61c16269ab0082d8c8b6e13f5f9470
Christian Fritsch <chr.fritsch@gmx.net>
2018-10-15 16:27:41 +0200

    Finish first autocomplete test

--- modules/select2_facets/tests/src/FunctionalJavascript/FacetsTest.php
+++ modules/select2_facets/tests/src/FunctionalJavascript/FacetsTest.php
@@ -21,9 +21,12 @@ class FacetsTest extends WebDriverTestBase {
    * Tests select2 autocomplete.
    */
   public function testAutocomplete() {
-    $reference1 = EntityTestMulRevPub::create(['name' => 'Reference 1'])->save();
-    $reference2 = EntityTestMulRevPub::create(['name' => 'Reference 2'])->save();
-    $reference3 = EntityTestMulRevPub::create(['name' => 'Reference 3'])->save();
+    $reference1 = EntityTestMulRevPub::create(['name' => 'Reference 1']);
+    $reference1->save();
+    $reference2 = EntityTestMulRevPub::create(['name' => 'Reference 2']);
+    $reference2->save();
+    $reference3 = EntityTestMulRevPub::create(['name' => 'Reference 3']);
+    $reference3->save();
     EntityTestMulRevPub::create([
       'name' => 'Entity 1',
       'field_references' => [$reference1, $reference2],
@@ -42,6 +45,21 @@ class FacetsTest extends WebDriverTestBase {
     $this->drupalPlaceBlock('facet_block:referenced');
 
     $this->drupalGet('/test-entity-view');
+
+    $page = $this->getSession()->getPage();
+    $assert_session = $this->assertSession();
+
+    $this->click('.form-item-referenced .select2-selection.select2-selection--multiple');
+    $page->find('css', '.select2-search__field')->setValue('Reference 2');
+    $page->find('xpath', '//li[@class="select2-results__option select2-results__option--highlighted"]')->click();
+
+    $assert_session->addressEquals('test-entity-view?f%5B0%5D=referenced%3A2');
+
+    $this->click('.form-item-referenced .select2-selection.select2-selection--multiple');
+    $page->find('css', '.select2-search__field')->setValue('Reference 1');
+    $page->find('xpath', '//li[@class="select2-results__option select2-results__option--highlighted"]')->click();
+
+    $assert_session->addressEquals('test-entity-view?f%5B0%5D=referenced%3A2&f%5B1%5D=referenced%3A1');
   }
 
 }

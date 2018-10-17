3af54be44c086b3c6c809034ea1ddf48ff41d47e
Christian Fritsch <chr.fritsch@gmx.net>
2018-10-17 16:29:24 +0200

    Cleanup

--- modules/select2_facets/src/Plugin/facets/widget/Select2Widget.php
+++ modules/select2_facets/src/Plugin/facets/widget/Select2Widget.php
@@ -107,7 +107,7 @@ class Select2Widget extends WidgetPluginBase {
       '#description' => $this->t("Define a width for the select2 field. It can be either 'element', 'style', 'resolve' or any possible CSS unit. E.g. 500px, 50%, 200em. See the <a href='https://select2.org/appearance#container-width'>select2 documentation</a> for further explanations."),
       '#required' => TRUE,
       '#size' => '10',
-      '#element_validate' => [[$this, 'validateWidth']],
+      '#pattern' => "^(\d+(cm|mm|in|px|pt|pc|em|ex|ch|rem|vm|vh|vmin|vmax|\%)|element|style|resolve)$",
     ];
     $form['autocomplete'] = [
       '#type' => 'checkbox',
@@ -129,20 +129,6 @@ class Select2Widget extends WidgetPluginBase {
     ];
 
     return $form;
-  }
-
-  /**
-   * Validate the width textfield.
-   *
-   * @param array $element
-   *   The form element.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The form state object.
-   */
-  public function validateWidth(array $element, FormStateInterface $form_state) {
-    if (!preg_match("/^(\d+(cm|mm|in|px|pt|pc|em|ex|ch|rem|vm|vh|vmin|vmax|\%)|element|style|resolve)$/i", $element['#value'])) {
-      $form_state->setError($element, $this->t('Width is not in a valid format.'));
-    }
   }
 
   /**

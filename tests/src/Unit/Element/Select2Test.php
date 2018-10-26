f13f3a6a7ccca289bbaa0f8c6cd69c7c1c684928
Christian Fritsch <chr.fritsch@gmx.net>
2018-10-26 10:12:21 +0200

    Start fixing tests

--- tests/src/Unit/Element/Select2Test.php
+++ tests/src/Unit/Element/Select2Test.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\select2\Unit\Element;
 
+use Drupal\Component\Serialization\Json;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\select2\Element\Select2;
 use Drupal\Tests\UnitTestCase;
@@ -45,10 +46,13 @@ class Select2Test extends UnitTestCase {
       ->method('getLibraryByName')
       ->will($this->returnValue(TRUE));
 
+    $string_translation = $this->createMock('Drupal\Core\StringTranslation\TranslationManager');
+
     $container = new ContainerBuilder();
     $container->set('language_manager', $language_manager);
     $container->set('theme.manager', $theme_manager);
     $container->set('library.discovery', $library_discovery);
+    $container->set('string_translation', $string_translation);
 
     \Drupal::setContainer($container);
   }
@@ -84,77 +88,104 @@ class Select2Test extends UnitTestCase {
     $data = [];
     $data[] = [TRUE, TRUE, [],
       [
-        '#attributes' => ['multiple' => 'multiple', 'name' => 'field_foo[]'],
-        '#attached' => [
-          'drupalSettings' => [
-            'select2' => [
-              'field-foo' => [
-                'multiple' => TRUE,
-                'allowClear' => FALSE,
-              ],
-            ],
-          ],
+        '#attributes' => [
+          'multiple' => 'multiple',
+          'name' => 'field_foo[]',
+          'data-select2-config' => Json::encode([
+            'multiple' => TRUE,
+            'placeholder' => '',
+            'allowClear' => FALSE,
+            'dir' => 'rtl',
+            'language' => 'en',
+            'tags' => FALSE,
+            'theme' => 'seven',
+            'maximumSelectionLength' => 0,
+            'tokenSeparators' => [],
+            'selectOnClose' => FALSE,
+            'width' => '100%',
+          ]),
         ],
       ],
     ];
     $data[] = [FALSE, TRUE, [],
       [
-        '#attributes' => [],
-        '#attached' => [
-          'drupalSettings' => [
-            'select2' => [
-              'field-foo' => [
-                'multiple' => FALSE,
-                'allowClear' => FALSE,
-              ],
-            ],
-          ],
+        '#attributes' => [
+          'name' => 'field_foo',
+          'data-select2-config' => Json::encode([
+            'multiple' => FALSE,
+            'placeholder' => '',
+            'allowClear' => FALSE,
+            'dir' => 'rtl',
+            'language' => 'en',
+            'tags' => FALSE,
+            'theme' => 'seven',
+            'maximumSelectionLength' => 0,
+            'tokenSeparators' => [],
+            'selectOnClose' => FALSE,
+            'width' => '100%',
+          ]),
         ],
       ],
     ];
     $data[] = [TRUE, FALSE, [],
       [
-        '#attributes' => [],
-        '#attached' => [
-          'drupalSettings' => [
-            'select2' => [
-              'field-foo' => [
-                'multiple' => TRUE,
-                'allowClear' => FALSE,
-              ],
-            ],
-          ],
+        '#attributes' => [
+          'multiple' => 'multiple',
+          'name' => 'field_foo[]',
+          'data-select2-config' => Json::encode([
+            'multiple' => TRUE,
+            'placeholder' => '',
+            'allowClear' => FALSE,
+            'dir' => 'rtl',
+            'language' => 'en',
+            'tags' => FALSE,
+            'theme' => 'seven',
+            'maximumSelectionLength' => 0,
+            'tokenSeparators' => [],
+            'selectOnClose' => FALSE,
+            'width' => '100%',
+          ]),
         ],
       ],
     ];
     $data[] = [FALSE, FALSE, [],
       [
-        '#attributes' => [],
-        '#attached' => [
-          'drupalSettings' => [
-            'select2' => [
-              'field-foo' => [
-                'multiple' => FALSE,
-                'allowClear' => TRUE,
-              ],
-            ],
-          ],
+        '#attributes' => [
+          'name' => 'field_foo',
+          'data-select2-config' => Json::encode([
+            'multiple' => FALSE,
+            'placeholder' => '',
+            'allowClear' => TRUE,
+            'dir' => 'rtl',
+            'language' => 'en',
+            'tags' => FALSE,
+            'theme' => 'seven',
+            'maximumSelectionLength' => 0,
+            'tokenSeparators' => [],
+            'selectOnClose' => FALSE,
+            'width' => '100%',
+          ]),
         ],
       ],
     ];
     // Test overwriting of the default setting.
     $data[] = [FALSE, FALSE, ['allowClear' => FALSE, 'multiple' => TRUE],
       [
-        '#attributes' => [],
-        '#attached' => [
-          'drupalSettings' => [
-            'select2' => [
-              'field-foo' => [
-                'multiple' => TRUE,
-                'allowClear' => FALSE,
-              ],
-            ],
-          ],
+        '#attributes' => [
+          'name' => 'field_foo',
+          'data-select2-config' => Json::encode([
+            'multiple' => TRUE,
+            'placeholder' => '',
+            'allowClear' => FALSE,
+            'dir' => 'rtl',
+            'language' => 'en',
+            'tags' => FALSE,
+            'theme' => 'seven',
+            'maximumSelectionLength' => 0,
+            'tokenSeparators' => [],
+            'selectOnClose' => FALSE,
+            'width' => '100%',
+          ]),
         ],
       ],
     ];

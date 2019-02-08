ac0027dad16aaee156ac076633ed41fb96ac05a6
Mladen Todorovic <mladen.todorovic@burda.com>
2019-02-08 13:09:37 +0100

    Add support for AJAX facets

--- modules/select2_facets/js/select2-widget.js
+++ modules/select2_facets/js/select2-widget.js
@@ -3,32 +3,29 @@
  * Init select2 widget.
  */
 
-(function ($) {
+(function ($, Drupal) {
 
   'use strict';
 
-  Drupal.facets = Drupal.facets || {};
+  /**
+   * Add event handler to all select2 widgets.
+   */
+  Drupal.facets.initSelect2 = function () {
+    $('.js-facets-select2').once('facets-select2').each(function () {
+      // Go to the selected option when it's clicked.
+      $(this).on('select2:select select2:unselect', function (item) {
+        $(this).trigger('facets_filter', [ item.params.data.id ]);
+      });
+    });
+  };
+
+  /**
+   * Behavior to register select2 widget to be used for facets.
+   */
   Drupal.behaviors.facetsSelect2Widget = {
     attach: function (context, settings) {
       Drupal.facets.initSelect2(context, settings);
     }
   };
 
-  /**
-   * Add event handler to all select2 widgets.
-   *
-   * @param {object} context
-   *   Context.
-   * @param {object} settings
-   *   Settings.
-   */
-  Drupal.facets.initSelect2 = function (context, settings) {
-    $('.js-facets-select2').once('facets-select2').each(function () {
-      // Go to the selected option when it's clicked.
-      $(this).on('select2:select select2:unselect', function (item) {
-        window.location.href = item.params.data.id;
-      });
-    });
-  };
-
-})(jQuery);
+})(jQuery, Drupal);

b3b3175cdea690c316c37146b61ba71d2192e2a5
Mladen Todorovic <mladen.todorovic@burda.com>
2019-02-21 13:24:24 +0100

    Add support for facets JS API from 3031581 #5

--- modules/select2_facets/js/select2-widget.js
+++ modules/select2_facets/js/select2-widget.js
@@ -13,12 +13,19 @@
    * Add event handler to all select2 widgets.
    */
   Drupal.facets.initSelect2 = function () {
-    $('.js-facets-select2').once('facets-select2').each(function () {
-      // Go to the selected option when it's clicked.
-      $(this).on('select2:select select2:unselect', function (item) {
-        $(this).trigger('facets_filter', [ item.params.data.id ]);
-      });
-    });
+    $('.js-facets-select2.js-facets-widget')
+      .once('js-facets-select2-widget-on-selection-change')
+      .each(function () {
+        var $select2_widget = $(this);
+
+        $select2_widget.on('select2:select select2:unselect', function (item) {
+          $select2_widget.trigger('facets_filter', [item.params.data.id]);
+        });
+
+        $select2_widget.on('facets_filtering.select2', function () {
+          $select2_widget.prop('disabled', true);
+        });
+      })
   };
 
   /**

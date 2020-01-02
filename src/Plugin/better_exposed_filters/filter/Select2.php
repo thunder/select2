560d6022820472b3303a7059d86ab73f50ffb036
Christian Fritsch <chr.fritsch@gmx.net>
2020-01-02 13:53:20 +0100

    Add schema

--- src/Plugin/better_exposed_filters/filter/Select2.php
+++ src/Plugin/better_exposed_filters/filter/Select2.php
@@ -18,47 +18,17 @@ class Select2 extends FilterWidgetBase {
   /**
    * {@inheritdoc}
    */
-  public static function isApplicable($filter = NULL, array $filter_options = []) {
-    /** @var \Drupal\views\Plugin\views\filter\FilterPluginBase $filter */
-    $is_applicable = FALSE;
-
-    // Sanity check to ensure we have a filter to work with.
-    if (!isset($filter)) {
-      return $is_applicable;
-    }
-
-    // Check various filter types and determine what options are available.
-    if (is_a($filter, 'Drupal\views\Plugin\views\filter\InOperator')) {
-      if (in_array($filter->operator, ['in', 'or', 'and', 'not'])) {
-        $is_applicable = TRUE;
-      }
-    }
-    if (is_a($filter, 'Drupal\taxonomy\Plugin\views\filter\TaxonomyIndexTid')) {
-      // Autocomplete and dropdown taxonomy filter are both instances of
-      // TaxonomyIndexTid, but we can't show BEF options for the autocomplete
-      // widget.
-      if ($filter_options['type'] == 'select') {
-        $is_applicable = TRUE;
-      }
-    }
-
-    return $is_applicable;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function exposedFormAlter(array &$form, FormStateInterface $form_state) {
-    /** @var \Drupal\views\Plugin\views\filter\FilterPluginBase $filter */
-    $filter = $this->handler;
-    // Form element is designated by the element ID which is user-
-    // configurable.
-    $field_id = $filter->options['expose']['identifier'];
+
+    $field_id = $this->getExposedFilterFieldId();
 
     parent::exposedFormAlter($form, $form_state);
 
     if (!empty($form[$field_id])) {
       $form[$field_id]['#type'] = 'select2';
+      $form[$field_id]['#select2'] = [
+        'width' => 'element',
+      ];
     }
   }
 

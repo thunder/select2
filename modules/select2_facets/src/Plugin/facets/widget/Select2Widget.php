47a0ef67f4efa9420d71037ce4625e8f1b164955
Christian Fritsch <chr.fritsch@gmx.net>
2020-01-28 16:43:23 +0100

    Foo

--- modules/select2_facets/src/Plugin/facets/widget/Select2Widget.php
+++ modules/select2_facets/src/Plugin/facets/widget/Select2Widget.php
@@ -44,6 +44,7 @@ class Select2Widget extends WidgetPluginBase implements ContainerFactoryPluginIn
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->request = $request;
     $this->keyValueStore = $key_value_store;
+    $foo = $_REQUEST;
   }
 
   /**

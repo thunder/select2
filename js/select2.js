ab3c120858357acd91e51412fdc57162a70f289b
Christian Fritsch <chr.fritsch@gmx.net>
2020-02-07 12:56:00 +0100

    feat: add select all button

--- js/select2.js
+++ js/select2.js
@@ -5,9 +5,49 @@
 (function ($, drupalSettings) {
   'use strict';
 
+  var passIt = function () {
+    function SelectAll() {}
+
+    SelectAll.prototype.render = function (decorated) {
+      var $rendered = decorated.call(this);
+      var self = this;
+
+      var $selectAll = $(
+        '<button type="button">Select All</button>'
+      );
+
+      $rendered.find('.select2-dropdown').prepend($selectAll);
+
+      $selectAll.on('click', function (e) {
+        var $results = $rendered.find('.select2-results__option[aria-selected=false]');
+
+        // Get all results that aren't selected
+        $results.each(function () {
+          var $result = $(this);
+
+          // Get the data object for it
+          var data = $result.data('data');
+
+          // Trigger the select event
+          self.trigger('select', {
+            data: data
+          });
+        });
+
+        self.trigger('close');
+      });
+
+      return $rendered;
+    }
+
+    return SelectAll;
+  }
+
+
   Drupal.behaviors.select2 = {
     attach: function (context) {
       $('.select2-widget', context).once('select2-init').each(function () {
+
         var config = $(this).data('select2-config');
         config.createTag = function (params) {
           var term = $.trim(params.term);
@@ -27,6 +67,22 @@
           }
           return option.text;
         };
+
+        $.fn.select2.amd.require([
+          'select2/utils',
+          'select2/dropdown',
+          'select2/dropdown/attachBody'
+        ], function (Utils, Dropdown, AttachBody) {
+          config.dropdownAdapter = Utils.Decorate(
+            Utils.Decorate(
+              Dropdown,
+              AttachBody
+            ),
+            passIt()
+          );
+
+        });
+
         $(this).data('select2-config', config);
 
         // Emit an event, that other modules have the chance to modify the

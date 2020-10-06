4fcb8eba36f92e2b92cbd6f7cf69749c05e02566
Volker Killesreiter <killesre@gmail.com>
2020-10-06 08:19:06 +0200

    Issue #3100219 by chr.fritsch, volkerk: Add a claro theme

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+const postcssCustomProperties = require('postcss-custom-properties');
+const postcssCalc = require('postcss-calc');
+const postcssDiscardComments = require('postcss-discard-comments');
+
+module.exports = {
+  plugins: [
+    postcssCustomProperties({
+      preserve: false,
+      importFrom: '../../../core/themes/claro/css/base/variables.pcss.css'
+    }),
+    postcssCalc,
+    postcssDiscardComments()
+  ]
+}
+

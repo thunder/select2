2c7bd15882dacfa1085010de0faeeb0fc3e2898d
Christian Fritsch <chr.fritsch@gmx.net>
2021-02-23 09:40:14 +0100

    Issue #3199822 by chr.fritsch: Update JS coding styles

--- modules/select2_publish/js/select2.publish.js
+++ modules/select2_publish/js/select2.publish.js
@@ -2,45 +2,48 @@
  * @file
  * Select2 integration.
  */
-(function ($, drupalSettings) {
-  'use strict';
-
+(($) => {
   Drupal.behaviors.select2_publish = {
-    attach: function (context) {
-      $('.select2-widget', context).on('select2-init', function (e) {
-        if (typeof $(e.target).data('select2-publish-default') === 'undefined') {
+    attach(context) {
+      $('.select2-widget', context).on('select2-init', (e) => {
+        if (
+          typeof $(e.target).data('select2-publish-default') === 'undefined'
+        ) {
           return;
         }
-        var config = $(e.target).data('select2-config');
+        const config = $(e.target).data('select2-config');
 
-        var parentCreateTagHandler = config.createTag;
-        config.createTag = function (params) {
-          var term = parentCreateTagHandler(params);
+        const parentCreateTagHandler = config.createTag;
+        config.createTag = (params) => {
+          const term = parentCreateTagHandler(params);
           if (term) {
             term.published = $(e.target).data('select2-publish-default');
           }
           return term;
         };
 
-        var templateHandlerWrapper = function (parentHandler) {
-          return function (option, item) {
+        const templateHandlerWrapper = (parentHandler) => {
+          return (option, item) => {
             if (parentHandler) {
               parentHandler(option, item);
             }
             if (item) {
-              var published = (option.published === true || $(option.element).attr('data-published') === 'true');
+              const published =
+                option.published === true ||
+                $(option.element).attr('data-published') === 'true';
               $(item).addClass(published ? 'published' : 'unpublished');
             }
             return option.text;
           };
         };
 
-        config.templateSelection = templateHandlerWrapper(config.templateSelection);
+        config.templateSelection = templateHandlerWrapper(
+          config.templateSelection,
+        );
         config.templateResult = templateHandlerWrapper(config.templateResult);
 
         $(e.target).data('select2-config', config);
       });
-    }
+    },
   };
-
 })(jQuery, drupalSettings);
